2e3494ad480aee94656f9bd9216665e9c8da976b
sarahpng <sarahwaseem21@gmail.com>
2024-12-30 01:53:57 +0500

    error handing and dialog box view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_with_flutter/constants/routes.dart';
 import 'package:notes_with_flutter/firebase_options.dart';
-import 'dart:developer' as devtools show log;
+
+import 'package:notes_with_flutter/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -73,10 +74,16 @@ class _LoginViewState extends State<LoginView> {
                   }
                 } on FirebaseAuthException catch (e) {
                   if (e.code == "user-not-found") {
-                    devtools.log("user not found");
+                    await showErrorDialog(context, "User not found");
                   } else if (e.code == "wrong-password") {
-                    devtools.log("wrong password");
+                    if (context.mounted) {
+                      await showErrorDialog(context, "Wrong credentials");
+                    }
+                  } else {
+                    await showErrorDialog(context, 'Error:${e.code}');
                   }
+                } catch (e) {
+                  await showErrorDialog(context, 'Error:${e.toString()}');
                 }
               },
               child: const Text("Login")
@@ -86,7 +93,7 @@ class _LoginViewState extends State<LoginView> {
           TextButton(
             onPressed: () {
               Navigator.of(context)
-                  .pushNamedAndRemoveUntil('/register/', (route) => false);
+                  .pushNamedAndRemoveUntil(registerRoute, (route) => false);
             },
             child: const Text("Not registered? Register here!"),
           ),
