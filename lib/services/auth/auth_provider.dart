3108a7745a8eb9931c85b23e3915e8f56382b36d
sarahpng <sarahwaseem21@gmail.com>
2025-01-04 22:49:53 +0500

    auth

---
+++ lib/services/auth/auth_provider.dart
@@ -0,0 +1,17 @@
+// auth provider class that encapsulates every provider that we may add in future
+
+import 'package:notes_with_flutter/services/auth/auth_user.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
